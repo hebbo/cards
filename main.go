ad30dcee9081e8208568c9638d59442dce87c159
Angel Pena Ruiz <angel@cultureamp.com>
2021-10-08 23:05:55 +1000

    Allow deck to be shuffled

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 func main() {
 	mydeck := newDeckFromFile("udemy_golang")
 
+	// fmt.Println("before shuffling...")
+	// mydeck.print()
+
+	mydeck.shuffle()
+
+	// fmt.Println("after shuffling...")
 	mydeck.print()
 }
