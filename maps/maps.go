ba8e1ccd276455f5f567c94fa07d929f25503ddf
Angel Pena Ruiz <angel@cultureamp.com>
2021-10-11 13:55:08 +1000

    iterate maps

--- maps/maps.go
+++ maps/maps.go
@@ -3,15 +3,16 @@ package maps
 import "fmt"
 
 func Call() {
-	empty := make(map[int]string)
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#36bcde",
 	}
 
-	empty[10] = "#ffffff"
-	delete(empty, 10)
+	printMap(colors)
+}
 
-	fmt.Println(colors)
-	fmt.Println(empty)
+func printMap(c map[string]string) {
+	for k, v := range c {
+		fmt.Println(k, v)
+	}
 }
